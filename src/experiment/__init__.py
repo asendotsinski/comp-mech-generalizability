7a9e4a782a7ba6482d768a2c7df949b0182eed8d
2t2c <your-email@example.com>
2025-01-09 17:37:27 +0100

    added OV module and logs

--- src/experiment/__init__.py
+++ src/experiment/__init__.py
@@ -3,3 +3,4 @@ from experiment.logit_lens import LogitLens, LogitStorage, HeadLogitStorage
 from experiment.ablation import Ablate
 from experiment.head_pattern import HeadPattern
 from experiment.ablator import Ablator
+from experiment.ov import OV
