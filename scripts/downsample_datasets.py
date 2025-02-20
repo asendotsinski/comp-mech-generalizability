19130581339d135ed1b09d8aa4eba1d2ec587eda
Marko Ivanov <marco.ivanov05@gmail.com>
2025-02-20 08:54:55 +0100

    Downsample script to support quake

--- scripts/downsample_datasets.py
+++ scripts/downsample_datasets.py
@@ -41,75 +41,28 @@ class DatasetEntry:
         return self.base_prompt == other.base_prompt and self.target_true == other.target_true and self.target_new == other.target_new and self.prompt == other.prompt
     
 
-def are_rows_equal(row1, row2):
-    base_prompt_equal = row1["base_prompt"] == row2["base_prompt"]
-    target_true_equal = row1["target_true"] == row2["target_true"]
-    target_new_equal = row1["target_new"] == row2["target_new"]
+def inference(prompt, model, tokenizer):
+    inputs = tokenizer(prompt, return_tensors="pt").to(device)
+    model_outputs = model.generate(**inputs, 
+                                max_new_tokens=1, 
+                                return_dict_in_generate=True, output_scores=True, 
+                                pad_token_id=tokenizer.eos_token_id, do_sample=False)
+    generated_tokens_ids = model_outputs.sequences[0]
+    generation = tokenizer.decode(generated_tokens_ids)
+    attribute = tokenizer.decode(generated_tokens_ids[-1])
 
-    return base_prompt_equal and target_true_equal and target_new_equal
+    return generation, attribute
 
-def main(inference_model_name, model_names):
-    hf_model_name = get_hf_model_name(inference_model_name)
-
-    # gpt2 inference
-    tokenizer = AutoTokenizer.from_pretrained(hf_model_name)
-    model = AutoModelForCausalLM.from_pretrained(
-            hf_model_name,
-            pad_token_id=tokenizer.eos_token_id,
-            device_map="auto",
-            do_sample=False)
-
-    def inference(prompt, model, tokenizer):
-        inputs = tokenizer(prompt, return_tensors="pt").to(device)
-        model_outputs = model.generate(**inputs, 
-                                    max_new_tokens=1, 
-                                    return_dict_in_generate=True, output_scores=True, 
-                                    pad_token_id=tokenizer.eos_token_id, do_sample=False)
-        generated_tokens_ids = model_outputs.sequences[0]
-        generation = tokenizer.decode(generated_tokens_ids)
-        attribute = tokenizer.decode(generated_tokens_ids[-1])
-
-        return generation, attribute
+def sequential_inference(model, tokenizer, dataset, prompt_key="prompt"):
+    generations, attributes = [], []
+    for row in tqdm(dataset):
+        generation, attribute = inference(row[prompt_key], model, tokenizer)
+        generations.append(generation.strip())
+        attributes.append(attribute.strip())
     
-    def sequential_inference(dataset, prompt_key="prompt", subset=None):
-        generations, attributes = [], []
-        for row in tqdm(dataset):
-            generation, attribute = inference(row[prompt_key], model, tokenizer)
-            generations.append(generation.strip())
-            attributes.append(attribute.strip())
-        
-        return generations, attributes
-    
-    datasets = {model_name: {} for model_name in model_names}
-    for model_name in model_names:
-        with open(f"../data/full_data_sampled_{model_name}_with_subjects.json", "r") as f:
-            datasets[model_name]["og"] = sorted(json.load(f), key=lambda x: x["prompt"])
+    return generations, attributes
 
-        with open(f"../data/cft_og_combined_data_sampled_{model_name}_with_questions.json", "r") as f:
-            datasets[model_name]["qa_cft"] = sorted(json.load(f), key=lambda x: x["prompt"])
-    
-    og_ground_truths_per_model, og_predictions_per_model = {}, {}
-    og_prompt_ground_truths_per_model, og_prompt_predictions_per_model = {}, {}
-    qa_cft_ground_truths_per_model, qa_cft_predictions_per_model = {}, {}
-    qa_cft_prompt_ground_truths_per_model, qa_cft_prompt_predictions_per_model = {}, {}
-    for model_name in model_names:
-        og_ground_truths, og_predictions = sequential_inference(datasets[model_name]["og"], prompt_key="base_prompt", subset=None)
-        og_ground_truths_per_model[model_name] = og_ground_truths
-        og_predictions_per_model[model_name] = og_predictions
-
-        og_prompt_ground_truths, og_prompt_predictions = sequential_inference(datasets[model_name]["og"], prompt_key="prompt", subset=None)
-        og_prompt_ground_truths_per_model[model_name] = og_prompt_ground_truths
-        og_prompt_predictions_per_model[model_name] = og_prompt_predictions
-
-        qa_cft_ground_truths, qa_cft_predictions = sequential_inference(datasets[model_name]["qa_cft"], prompt_key="base_prompt", subset=None)
-        qa_cft_ground_truths_per_model[model_name] = qa_cft_ground_truths
-        qa_cft_predictions_per_model[model_name] = qa_cft_predictions
-
-        qa_cft_prompt_ground_truths, qa_cft_prompt_predictions = sequential_inference(datasets[model_name]["qa_cft"], prompt_key="prompt", subset=None)
-        qa_cft_prompt_ground_truths_per_model[model_name] = qa_cft_prompt_ground_truths
-        qa_cft_prompt_predictions_per_model[model_name] = qa_cft_prompt_predictions
-
-    def check_qa_stats(dataset, ground_truths, predictions):    
+def check_qa_stats(dataset, ground_truths, predictions):    
         target_new = np.array([row["target_new"].strip() for row in dataset])
         target_true = np.array([row["target_true"].strip() for row in dataset])
 
@@ -135,6 +88,98 @@ def main(inference_model_name, model_names):
         print("Total invalid Indices:", len(invalid_indices))
 
         return fact_indices, cofact_indices, indices, invalid_indices
+
+def downsample_mquake(inference_model_name):
+    hf_model_name = get_hf_model_name(inference_model_name)
+
+    # gpt2 inference
+    tokenizer = AutoTokenizer.from_pretrained(hf_model_name)
+    model = AutoModelForCausalLM.from_pretrained(
+            hf_model_name,
+            pad_token_id=tokenizer.eos_token_id,
+            device_map="auto",
+            do_sample=False)
+    
+    # Load the dataset
+    dataset = None
+    with open(f"../data/mquake_dataset_combined.json", "r") as f:
+        dataset = sorted(json.load(f), key=lambda x: x["prompt"])
+
+    base_prompt_ground_truths, base_prompt_predictions = sequential_inference(model, tokenizer, dataset, prompt_key="base_prompt")
+    prompt_ground_truths, prompt_predictions = sequential_inference(model, tokenizer, dataset, prompt_key="prompt")
+    questions_ground_truths, questions_predictions = sequential_inference(model, tokenizer, dataset, prompt_key="question")
+
+    print("=" * 100)
+    print("Base Prompt Stats:")
+    base_prompt_fact_indices, base_prompt_cofact_indices, base_prompt_indices, base_prompt_invalid_indices = check_qa_stats(dataset, 
+                                                                                    base_prompt_ground_truths, 
+                                                                                    base_prompt_predictions)
+    
+    print("=" * 100)
+    print("Prompt stats:")
+    prompt_fact_indices, prompt_cofact_indices, prompt_indices, prompt_invalid_indices = check_qa_stats(dataset, 
+                                                                                    prompt_ground_truths, 
+                                                                                    prompt_predictions)
+    
+    og_dataset_new = []
+    dataset_array = np.array(dataset)
+
+    # Convert to sorted lists for deterministic behavior
+    og_indices = sorted(list(set(base_prompt_fact_indices) & 
+                           set(prompt_indices)))
+    
+    for idx in og_indices:
+        row = DatasetEntry(dataset_array[idx])
+        if row in og_dataset_new:
+            continue
+        og_dataset_new.append(row)
+    
+    og_dataset_new = [row.row for row in og_dataset_new]
+
+    print(f'Final og dataset length: {len(og_dataset_new)}')
+
+    with open(f"../data/full_data_sampled_{inference_model_name}_with_subjects_downsampled.json", "w") as f:
+        json.dump(og_dataset_new, f)
+
+def main(inference_model_name, model_names):
+    hf_model_name = get_hf_model_name(inference_model_name)
+
+    # gpt2 inference
+    tokenizer = AutoTokenizer.from_pretrained(hf_model_name)
+    model = AutoModelForCausalLM.from_pretrained(
+            hf_model_name,
+            pad_token_id=tokenizer.eos_token_id,
+            device_map="auto",
+            do_sample=False)
+
+    datasets = {model_name: {} for model_name in model_names}
+    for model_name in model_names:
+        with open(f"../data/full_data_sampled_{model_name}_with_subjects.json", "r") as f:
+            datasets[model_name]["og"] = sorted(json.load(f), key=lambda x: x["prompt"])
+
+        with open(f"../data/cft_og_combined_data_sampled_{model_name}_with_questions.json", "r") as f:
+            datasets[model_name]["qa_cft"] = sorted(json.load(f), key=lambda x: x["prompt"])
+    
+    og_ground_truths_per_model, og_predictions_per_model = {}, {}
+    og_prompt_ground_truths_per_model, og_prompt_predictions_per_model = {}, {}
+    qa_cft_ground_truths_per_model, qa_cft_predictions_per_model = {}, {}
+    qa_cft_prompt_ground_truths_per_model, qa_cft_prompt_predictions_per_model = {}, {}
+    for model_name in model_names:
+        og_ground_truths, og_predictions = sequential_inference(model, tokenizer, datasets[model_name]["og"], prompt_key="base_prompt")
+        og_ground_truths_per_model[model_name] = og_ground_truths
+        og_predictions_per_model[model_name] = og_predictions
+
+        og_prompt_ground_truths, og_prompt_predictions = sequential_inference(model, tokenizer, datasets[model_name]["og"], prompt_key="prompt")
+        og_prompt_ground_truths_per_model[model_name] = og_prompt_ground_truths
+        og_prompt_predictions_per_model[model_name] = og_prompt_predictions
+
+        qa_cft_ground_truths, qa_cft_predictions = sequential_inference(model, tokenizer, datasets[model_name]["qa_cft"], prompt_key="base_prompt")
+        qa_cft_ground_truths_per_model[model_name] = qa_cft_ground_truths
+        qa_cft_predictions_per_model[model_name] = qa_cft_predictions
+
+        qa_cft_prompt_ground_truths, qa_cft_prompt_predictions = sequential_inference(model, tokenizer, datasets[model_name]["qa_cft"], prompt_key="prompt")
+        qa_cft_prompt_ground_truths_per_model[model_name] = qa_cft_prompt_ground_truths
+        qa_cft_prompt_predictions_per_model[model_name] = qa_cft_prompt_predictions
 
     og_fact_indices_per_model, og_cofact_indices_per_model, og_indices_per_model, og_invalid_indices_per_model = {}, {}, {}, {}
     og_prompt_fact_indices_per_model, og_prompt_cofact_indices_per_model, og_prompt_indices_per_model, og_prompt_invalid_indices_per_model = {}, {}, {}, {}
@@ -216,10 +261,14 @@ if __name__ == "__main__":
     parser = ArgumentParser()
 
     parser.add_argument("--model", type=str, default="gpt2")
+    parser.add_argument("--use_mquake", action="store_true", default=False)
     parser.add_argument('--models', nargs='+', type=str, default=['gpt2'], help='List of model datasets to use')
 
     args = parser.parse_args()
 
     print(args)
 
-    main(args.model, args.models)
+    if args.use_mquake:
+        downsample_mquake(args.model)
+    else:
+        main(args.model, args.models)
